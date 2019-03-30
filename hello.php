ad37d298c606ee03bb0dbf9c7ebd1b2381732185
tomiiyukihiro <46737333+tomiiyukihiro@users.noreply.github.com>
2019-03-31 00:23:29 +0900

    Update hello.php

--- hello.php
+++ hello.php
@@ -2,9 +2,13 @@
 function hello($event) {
 	if (empty($event->message->text)) return;
 
-	if (!preg_match('/こんにちは/', $event->message->text)) return;
+	if (preg_match('/こんにちは/',     $event->message->text)) reply($event, 'こんにちは');
+	if (preg_match('/こんにちわ/',     $event->message->text)) reply($event, 'こんにちは');
+	if (preg_match('/おはよう/',       $event->message->text)) reply($event, 'おはようございます');
+	if (preg_match('/こんばんは/',     $event->message->text)) reply($event, 'こんばんは');
+	if (preg_match('/こんばんわ/',     $event->message->text)) reply($event, 'こんばんは');
+	if (preg_match('/おやすみなさい/', $event->message->text)) reply($event, 'おやすみなさい');
 
-	reply($event, 'こんにちは');
 }
 
 ?>
