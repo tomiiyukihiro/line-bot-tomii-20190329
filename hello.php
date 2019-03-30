faa0ecbcff16cafca5fa8126a626a6e2530f8cdd
tomiiyukihiro <46737333+tomiiyukihiro@users.noreply.github.com>
2019-03-30 23:53:10 +0900

    Update hello.php

--- hello.php
+++ hello.php
@@ -3,9 +3,9 @@ function hello($event) {
 	debug('event', $event);
 	if (empty($event->message->text)) return;
 
-	if (!preg_match('/‚±‚ñ‚É‚¿‚Í/', $event->message->text)) return;
+	if (!preg_match('/ã“ã‚“ã«ã¡ã¯/', $event->message->text)) return;
 
-	reply($event, '‚±‚ñ‚É‚¿‚Í');
+	reply($event, 'ã“ã‚“ã«ã¡ã¯');
 }
 
 ?>
