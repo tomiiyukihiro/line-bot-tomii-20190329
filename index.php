aa348493bfae1e0f60b91e7eb7a2831906c8f1b0
tomiiyukihiro <46737333+tomiiyukihiro@users.noreply.github.com>
2019-03-30 23:49:01 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,20 @@
 <?php
-require_once('weather.php');
+
 define('DEBUG', 'debug.txt');
 require_once('tool.php');
 $input=file_get_contents('php://input');
 debug('input', $input);
+
+require_once('weather.php');
+require_once('hello.php');
+
 if (!empty($input)) {
 	$events=json_decode($input)->events;
 	foreach ($events as $event) {
 		//if (preg_match('/“V‹C/', $event->message->text)) weather($event);
 		weather($event);
 		//bot($event);
+		hello($event);
 	}
 }
 
