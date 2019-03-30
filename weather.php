19c762a7c214a0131467db3501882642993aea22
tomiiyukihiro <46737333+tomiiyukihiro@users.noreply.github.com>
2019-03-30 21:34:49 +0900

    Create weather.php

---
+++ weather.php
@@ -0,0 +1,85 @@
+<?php
+//define('DEBUG','debug.txt');
+//$input=file_get_contents('php://input');
+//file_put_contents(DEBUG, $input);
+//
+//function city_id($text) {
+//	$city=[
+//		'北海道'=>'016010',
+//		'青森'=>'020010',
+//		'岩手'=>'030010',
+//		'宮城'=>'040010',
+//		'秋田'=>'050010',
+//		'山形'=>'060010',
+//		'福島'=>'070010',
+//		'茨城'=>'080010',
+//		'栃木'=>'090010',
+//		'群馬'=>'100010',
+//		'埼玉'=>'110010',
+//		'千葉'=>'120010',
+//		'東京'=>'130010',
+//		'神奈川'=>'140010',
+//		'新潟'=>'150010',
+//		'富山'=>'160010',
+//		'石川'=>'170010',
+//		'福井'=>'180010',
+//		'山梨'=>'190010',
+//		'長野'=>'200010',
+//		'岐阜'=>'210010',
+//		'静岡'=>'220010',
+//		'愛知'=>'230010',
+//		'三重'=>'240010',
+//		'滋賀'=>'250010',
+//		'京都'=>'260010',
+//		'大阪'=>'270000',
+//		'兵庫'=>'280010',
+//		'奈良'=>'290010',
+//		'和歌山'=>'300010',
+//		'鳥取'=>'310010',
+//		'島根'=>'320010',
+//		'岡山'=>'330010',
+//		'広島'=>'340010',
+//		'山口'=>'350010',
+//		'徳島'=>'360010',
+//		'香川'=>'370000',
+//		'愛媛'=>'380010',
+//		'高知'=>'390010',
+//		'福岡'=>'400010',
+//		'佐賀'=>'410010',
+//		'長崎'=>'420010',
+//		'熊本'=>'430010',
+//		'大分'=>'440010',
+//		'宮崎'=>'450010',
+//		'鹿児島'=>'460010',
+//		'沖縄'=>'470010',
+//	];
+//	foreach ($city as $name=>$id) {
+//		if (preg_match('/'.$name.'/', $text)) return $id;
+//file_put_contents(DEBUG, $name);
+//	}
+//}
+
+function bot($event) {
+	//if (empty($event->message->text)) return;
+
+	//if (!preg_match('/天気/', $event->message->text)) {
+	//	return;
+	//}
+
+	//$id=city_id($event->message->text);
+	//if (empty($id)) return;
+
+	//$weather=load('http://weather.livedoor.com/forecast/webservice/json/v1?city='.$id);
+	//$weather=load('http://weather.livedoor.com/forecast/webservice/json/v1?city=130010');
+	//$weather=load('http://weather.livedoor.com/forecast/webservice/json/v1?city=130010');
+	//$text=$weather->location->city."の天気は\n";
+	//foreach ($weather->forecasts as $forecast) {
+	//	$text.=$forecast->dateLabel.' '.$forecast->telop."\n";
+	//}
+	//$text.='です。';
+	//reply($event, $text);
+	//reply($event, $weather->description->$text);
+	reply($event, 'こんにちは');
+}
+
+?>
