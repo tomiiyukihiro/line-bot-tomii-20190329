9f534049dd76eb6f6d0feac47645f690c9f5c067
tomiiyukihiro <46737333+tomiiyukihiro@users.noreply.github.com>
2019-03-31 00:03:25 +0900

    Create history.php

---
+++ history.php
@@ -0,0 +1,10 @@
+<?php
+function history($event) {
+	if (empty($event->message->text)) return;
+
+	if (!preg_match('/•ÏX—š—ð/', $event->message->text)) return;
+
+	reply($event, '2019/03/29ì¬ \n2019/03/30“V‹C \n');
+}
+
+?>
