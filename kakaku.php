4999d65063a8712afd1d9f99890d29ef3dbe5e67
tomiiyukihiro <46737333+tomiiyukihiro@users.noreply.github.com>
2019-04-01 01:03:22 +0900

    Create kakaku.php

---
+++ kakaku.php
@@ -0,0 +1,23 @@
+<?php
+function kakaku($event) {
+	$id='1023924760830020175';
+	if (!preg_match('/価格検索/', $event->message->text)) return;
+	$keyword=preg_replace('/価格検索/', '', $event->message->text);
+	$url='https://app.rakuten.co.jp/services/api/IchibaItem/Search/20140222';
+	$url.='?applicationId='.$id;
+	$url.='&keyword='.urlencode($keyword);
+	$url.='&sort='.urlencode('+itemPrice');
+	$url.='&elements=itemName,itemPrice,itemUrl';
+	$url.='&hits=3';
+	debug('url', $url);
+
+	$result=load($url);
+	$text='「'.$keyword."」の検索結果です。\n\n";
+	foreach ($result->Items as $item) {
+		$text.=mb_substr($item->Item->itemName, 0, 40)."...\n";
+		$text.=$item->Item->itemProce."円\n";
+		$text.=$item->Item->itemUrl."\n\n";
+	}
+	reply($event, $text);
+}
+?>
