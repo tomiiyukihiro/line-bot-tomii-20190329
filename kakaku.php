caeb26a8eca8e045c3418e2f78743ba6f6e9dabd
tomiiyukihiro <46737333+tomiiyukihiro@users.noreply.github.com>
2019-04-01 01:24:45 +0900

    Update kakaku.php

--- kakaku.php
+++ kakaku.php
@@ -16,7 +16,7 @@ function kakaku($event) {
 	foreach ($result->Items as $item) {
 		//$text.=mb_substr($item->Item->itemName, 0, 40)."...\n";
 		$text.=$item->Item->itemName."\n";
-		$text.=$item->Item->itemProce."円\n";
+		$text.=$item->Item->itemPrice."円\n";
 		$text.=$item->Item->itemUrl."\n\n";
 	}
 	reply($event, $text);
