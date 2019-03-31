078c5c4e168d33545e183dcc247e8b706f0adc73
tomiiyukihiro <46737333+tomiiyukihiro@users.noreply.github.com>
2019-04-01 02:53:24 +0900

    Update history.php

--- history.php
+++ history.php
@@ -5,7 +5,7 @@ function history($event) {
 	if (!preg_match('/変更履歴/', $event->message->text)) return;
 
 	$text=load('modify.txt');
-	reply($event, $text);
+	reply($event, $text[]);
 
 	//reply($event, '2019/03/29作成 \n2019/03/30天気 \n');
 }
