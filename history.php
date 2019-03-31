fac8303588a282d6f8fb2af595353e8190a89c06
tomiiyukihiro <46737333+tomiiyukihiro@users.noreply.github.com>
2019-04-01 02:57:02 +0900

    Update history.php

--- history.php
+++ history.php
@@ -4,10 +4,13 @@ function history($event) {
 
 	if (!preg_match('/変更履歴/', $event->message->text)) return;
 
-	$text=load('modify.txt');
-	reply($event, $text[0]);
+	//$text=load('modify.txt');
+	//reply($event, $text[0]);
 
-	//reply($event, '2019/03/29作成 \n2019/03/30天気 \n');
+	reply($event, "
+		2019/03/29作成 \n
+		2019/03/30天気 \n
+	");
 }
 
 ?>
